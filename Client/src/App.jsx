83be667e7814016e19594bd6f59036efba6d1567
Michael Testut <michael.testut@yahoo.com>
2023-03-31 22:25:35 -0700

    initial setup

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path = "/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
