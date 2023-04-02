a01151810b23293e5849e7a97c8f43168a57f003
Michael Testut <michael.testut@yahoo.com>
2023-04-01 17:54:19 -0700

    start home  page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
+import Background from "../assets/Background.jpeg"
 
 export default function Home() {
   return (
-    <div>Home</div>
+      <Container>
+          <div className="front-page">  
+              <img src={Background} alt="background image" className="background-image" />
+              <h2 className='subtitle'>Welcome to</h2>
+          </div>
+      </Container>
   )
 }
+
+const Container = styled.div`
+    .front-page{
+        position:relative;
+        display:flex;
+    }
+    .background-image{
+        height:100vh;
+        width:100vw;
+    }
+    .subtitle{
+        color: white;
+        text-align: center;
+        position: absolute;
+        left:0px;
+        right: 0px;
+        top: 100px;
+
+    }
+
+`
