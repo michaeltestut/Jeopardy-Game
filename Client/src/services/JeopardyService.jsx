48b6769884bc96c0a9054c6f0dfdc8812716fb69
Michael Testut <michael.testut@yahoo.com>
2023-05-04 21:13:42 -0700

    clean up code

--- Client/src/services/JeopardyService.jsx
+++ Client/src/services/JeopardyService.jsx
@@ -1,15 +1,9 @@
-import axios, { all } from 'axios'
+import axios from 'axios'
 
 export const fetchCategories = async () => {
-    const categoriesArray = [];
     try {
         const response = await axios.get('http://jservice.io/api/categories?count=6');
         const allCategories = response.data;
-        // for (var i = 0; i < allCategories.length; i++) {
-        //     if (categoriesArray.length < 6) {
-        //         categoriesArray.push(allCategories[i].title)
-        //     }
-        // }
         return allCategories
     } catch (error) {
         console.log(error)
